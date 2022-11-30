0fbc745540ba074e1863b3a35f90fda95952ca9d
Tman99877 <wallacetyrese8@gmail.com>
2022-11-30 02:48:48 +0000

    up33

---
+++ actionpage.php
@@ -0,0 +1,4 @@
+<?php
+$id = $_POST["fname"];
+print_r($_POST);
+?>
--- storage.py
+++ storage.py
@@ -66,4 +66,4 @@ def ps():
                                            
     
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8080,use_reloader=False)
